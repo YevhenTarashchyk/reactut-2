262010044fa7b33a80db841ad836f022acb670d5
Yevhen_Tarashchyk <maze.ultramarine@gmail.com>
2019-08-06 12:03:52 +0300

    initial commit

---
+++ src/app/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Route, Link, HashRouter } from 'react-router-dom';
+
+function App() {
+  return <div className="App" />;
+}
+
+export default App;
