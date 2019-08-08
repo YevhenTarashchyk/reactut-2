c9661a672edaa5a0e182a865e501c6716168eab6
Yevhen_Tarashchyk <maze.ultramarine@gmail.com>
2019-08-08 09:35:20 +0300

    Update Info.js

--- src/app/components/Info.js
+++ src/app/components/Info.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 const Info = props => (
   <div>
-    <h2 className="title-container__title">Погодное приложение</h2>
+    <h2 className="title-container__title">Weather app</h2>
     <h3 className="title-container__subtitle">Узнайте погоду в вашем городе</h3>
   </div>
 );
